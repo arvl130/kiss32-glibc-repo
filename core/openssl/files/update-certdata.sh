99620bae1f2b72ffe581fef5dfa60c985b4b7300
Angelo Geulin <angelogeulin123@gmail.com>
2020-04-06 13:07:29 +0800

    Initial commit

---
+++ core/openssl/files/update-certdata.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+#
+# update-certdata.sh
+
+[ -w /etc/ssl ] || {
+    printf '%s\n' "${0##*/}: root required to update cert." >&2
+    exit 1
+}
+
+cd /etc/ssl && {
+    curl https://curl.haxx.se/ca/cacert.pem -o cacert.pem
+    mv -f cacert.pem cert.pem
+    printf '%s\n' "${0##*/}: updated cert.pm"
+}
